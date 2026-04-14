69c8c8e0ea956b6e8239a11e5ab9a707aaede555
deceibe_the_world <alisherbolotbekov03@gmail.com>
2026-04-14 18:24:44 +0600

    version

---
+++ python.py
@@ -0,0 +1,30 @@
+# class User:
+#     def study(self):
+#         print("учится")
+
+# class Student(User):
+#     pass
+
+# class Animal:
+#     def speak(self):
+#         pass
+
+# class Dog(Animal):
+#     def speak(self):
+#         print("Гав-гав")
+
+# class Cat(Animal):
+#     def speak(self):
+#         print("Мяу-мяу")
+
+# class Cow(Animal):
+#     def speak(self):
+#         print("Му-му")
+
+# class Cafe:
+#     def __init__(self, name):
+#         self.name = name
+
+#     def order(self, dish):
+#         print(f"Вы заказали {dish} в кафе {self.name}")
+
